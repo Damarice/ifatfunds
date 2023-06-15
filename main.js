b84867706a85e4dde107401e93f548612c3434b5
Damarice <menganyidamarice@gmail.com>
2023-06-15 19:44:57 +0300

    change

--- main.js
+++ main.js
@@ -15,3 +15,16 @@ document.addEventListener('DOMContentLoaded', function() {
     alert('Form submitted!'); // replace with your actual form handling code
   });
 });
+// Update progress bar width based on project progress
+function updateProgressBar(projectName, progress) {
+  var progressBar = document.getElementById(projectName + "-progress");
+  progressBar.style.width = progress + "%";
+}
+
+// Update progress bar for each project
+updateProgressBar("charityA", 60); // Progress for Charity A (60%)
+updateProgressBar("projectX", 25); // Progress for Project X (25%)
+updateProgressBar("charityB", 90); // Progress for Charity B (90%)
+updateProgressBar("charityC", 40); // Progress for Charity C (40%)
+updateProgressBar("projectY", 75); // Progress for Project Y (75%)
+
